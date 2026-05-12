4006f834cc1fa15216d262cbe3d6d8472a2abb24
Keeper <keeper28@o2.pl>
2026-05-12 19:19:51 +0200

    fix drag ComboBox by DragAndDropBehavior

---
+++ KeeperDataGridAvalonia/Behaviors/DragAndDropBehavior.cs
@@ -0,0 +1,28 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+namespace KeeperDataGridAvalonia.Behaviors;
+
+public static class DragAndDropBehavior
+{
+    public static readonly AttachedProperty<bool> BlockGridDragProperty =
+        AvaloniaProperty.RegisterAttached<Control, bool>(
+            "BlockGridDrag",
+            typeof(DragAndDropBehavior));
+
+    static DragAndDropBehavior()
+    {
+        BlockGridDragProperty.Changed.AddClassHandler<Control>((ctrl, e) =>
+        {
+            if ((bool)e.NewValue!)
+            {
+                ctrl.PointerPressed += (_, ev) => ev.Handled = true;
+            }
+        });
+    }
+
+    public static void SetBlockGridDrag(AvaloniaObject element, bool value)
+        => element.SetValue(BlockGridDragProperty, value);
+
+    public static bool GetBlockGridDrag(AvaloniaObject element)
+        => element.GetValue(BlockGridDragProperty);
+}
